94084e6716c2be420831fd272a50718ba00ca29f
Yue030 <64401885+Yue030@users.noreply.github.com>
2021-10-15 18:38:53 +0800

    將發布通知的規則更改
    將原先每10秒檢測到一有更動，就進行發布訊息的機制
    
    更改為
    
    每10秒檢測是否有到達使用者設定的通知數，若有才會發通知

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,13 @@
 import servers from '~/secret/server.json';
+import notify from '~/secret/notify.json';
 import global from '@/global';
+import fs from 'fs';
 
 import { getChannelList } from '@/google/youtube';
 
 import config from '~/secret/config.json';
+
+const notifyFileName = './secret/notify.json';
 
 const ready = (client: any, Discord: any) => {
   console.log('Connected');
@@ -15,16 +19,31 @@ const ready = (client: any, Discord: any) => {
       id: [config['yt-channel-id']],
       part: ['statistics', 'snippet'],
     })
-      .then((res) => {
+      .then(async (res) => {
         if (!res?.items) throw 'response.items not available!';
         const ytChannel = res.items[0];
         if (!ytChannel) throw 'No channel found!';
         if (!ytChannel.snippet) throw 'channel.snippet not available!';
         if (!ytChannel.snippet.title)
           throw 'channel.snippet.title not available!';
+
         global.channel_name = ytChannel.snippet.title;
-        for (const server_info of servers as any) {
-          if (server_info.channel_id.trim() === '') continue;
+
+        if (!ytChannel.statistics) throw 'channel.statistics not available!';
+          if (!ytChannel.statistics.subscriberCount)
+            throw 'channel.statistics.subscriberCount not available!';
+
+        const subCount = Number(ytChannel.statistics.subscriberCount);
+
+        for (const notify_info of notify as any) {
+          if (Number(notify_info.notify_count) > subCount) continue;
+
+          if (notify_info.server_id.trim() == '') continue;
+          
+          const server = servers.filter(x => x.server_id == notify_info.server_id);
+          if (server.length < 1) continue;
+
+          const server_info = server[0];
 
           const guild = client.guilds.cache.get(server_info.server_id);
           if (!guild) continue;
@@ -32,22 +51,17 @@ const ready = (client: any, Discord: any) => {
           const channel = guild.channels.cache.get(server_info.channel_id);
           if (!channel) continue;
 
-          if (!ytChannel.statistics) throw 'channel.statistics not available!';
-          if (!ytChannel.statistics.subscriberCount)
-            throw 'channel.statistics.subscriberCount not available!';
-          const subCount = Number(ytChannel.statistics.subscriberCount);
-          if (subCount > global.current_count) {
-            channel.send(
-              `${global.channel_name} 的訂閱數: ${Number(
-                ytChannel.statistics.subscriberCount,
-              )}`,
-            );
-            console.log(`Send to ${server_info.server_id}`);
-          }
-        }
+          await channel.send(
+            `${global.channel_name} 的訂閱數: ${subCount}`,
+          );
 
-        if (!ytChannel.statistics) throw 'channel.statistics not available!';
-        global.current_count = Number(ytChannel.statistics.subscriberCount);
+          removeItemOnce(notify, notify_info);
+
+          console.log(`Send to ${server_info.server_id}`);
+
+          fs.writeFileSync(notifyFileName, JSON.stringify(notify, null, 2));
+        }
+        global.current_count = subCount;
       })
       .catch((err) => {
         console.log('錯誤:', err);
@@ -57,5 +71,13 @@ const ready = (client: any, Discord: any) => {
   checkYTCount();
   setInterval(() => checkYTCount(), 10000);
 };
+
+function removeItemOnce(arr : Array<any>, value : any) {
+  var index = arr.indexOf(value);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+}
 
 export default ready;
