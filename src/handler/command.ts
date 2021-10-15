9a0488c888e52c99a584d9d5f2985353424aa684
Yue030 <64401885+Yue030@users.noreply.github.com>
2021-10-15 18:36:43 +0800

    新增Notify指令
    用於設定到達指定訂閱數的時候
    進行通知

--- src/handler/command.ts
+++ src/handler/command.ts
@@ -2,12 +2,13 @@ import { Collection } from 'discord.js';
 
 import count from '@/commands/features/count';
 import set from '@/commands/features/set';
+import notify from '@/commands/features/notify';
 
 const Commands = (client: any, Discord: any): void => {
   client.commands = new Collection();
   client.aliases = new Collection();
 
-  [count, set].forEach((cmd: any) => {
+  [count, set, notify].forEach((cmd: any) => {
     if (cmd.name) {
       client.commands.set(cmd.name, cmd);
       if (cmd.aliases && Array.isArray(cmd.aliases)) {
